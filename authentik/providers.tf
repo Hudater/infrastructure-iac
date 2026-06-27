1b7a16112f9b2eac20d7074f0384ae0eb33c5734
Harshit Mani Tripathi <harshit@hudater.dev>
2026-06-28 03:21:46 +0530

    extend token ttls for netbird

--- authentik/providers.tf
+++ authentik/providers.tf
@@ -164,9 +164,9 @@ resource "authentik_provider_oauth2" "netbird" {
   authorization_flow     = data.authentik_flow.default_authorization.id
   invalidation_flow      = data.authentik_flow.default_invalidation.id
   signing_key            = data.authentik_certificate_key_pair.default.id
-  access_token_validity  = "hours=24"
-  refresh_token_validity = "days=180"
-  logout_uri             = "https://netbird.${local.domain}/"
+  access_token_validity  = "days=30"
+  refresh_token_validity = "days=365"
+  logout_uri             = "https://netbird.${local.domain}/oauth2/logout/callback"
 
   allowed_redirect_uris = [
     {
