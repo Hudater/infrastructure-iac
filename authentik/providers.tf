e1d12a5b4213087575d9415ee5ce5daf1aa6de31
Harshit Mani Tripathi <harshit@hudater.dev>
2026-07-07 23:13:59 +0530

    add: arcane app and provider

--- authentik/providers.tf
+++ authentik/providers.tf
@@ -219,3 +219,37 @@ resource "authentik_application" "bom_arm_sparky" {
   slug              = "bom-arm-sparky"
   protocol_provider = authentik_provider_oauth2.bom_arm_sparky.id
 }
+
+## Arcane
+resource "authentik_provider_oauth2" "arcane_bom_arm" {
+  name          = "arcane-bom-arm"
+  client_id     = "arcane_bom-arm"
+  # client_secret = var.arcane_bom_arm_secret
+
+  authorization_flow = data.authentik_flow.default_authorization.id
+  invalidation_flow  = data.authentik_flow.default_invalidation.id
+  signing_key        = data.authentik_certificate_key_pair.default.id
+
+  logout_uri = "https://arcane.${local.domain}/"
+
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict"
+      url           = "https://arcane.${local.domain}/"
+    }
+  ]
+
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.openid.id,
+    data.authentik_property_mapping_provider_scope.email.id,
+    data.authentik_property_mapping_provider_scope.profile.id,
+    data.authentik_property_mapping_provider_scope.entitlements.id,
+    authentik_property_mapping_provider_scope.groups.id,
+  ]
+}
+
+resource "authentik_application" "arcane_bom_arm" {
+  name              = "arcane"
+  slug              = "arcane-bom-arm"
+  protocol_provider = authentik_provider_oauth2.arcane_bom_arm.id
+}
