cdcd35f973a7d000a2da28580548cc25a9e849c7
Harshit Mani Tripathi <harshit@hudater.dev>
2026-07-10 18:27:15 +0530

    fix: arcane provider and mapping

--- authentik/provider_arcane.tf
+++ authentik/provider_arcane.tf
@@ -1,6 +1,18 @@
+resource "authentik_property_mapping_provider_scope" "arcane_email" {
+  name       = "scope-email-arcane"
+  scope_name = "email"
+  expression = <<-EOT
+    return {
+        "email": request.user.email,
+        "email_verified": True,
+    }
+  EOT
+}
+
 resource "authentik_provider_oauth2" "arcane_bom_arm" {
   name          = "arcane-bom-arm"
   client_id     = "arcane_bom-arm"
+  client_type   = "public"
   # client_secret = var.arcane_bom_arm_secret
 
   authorization_flow = data.authentik_flow.default_authorization.id
@@ -8,7 +20,7 @@ resource "authentik_provider_oauth2" "arcane_bom_arm" {
   signing_key        = data.authentik_certificate_key_pair.default.id
 
   logout_uri = "https://arcane.${local.domain}/"
-
+  sub_mode = "hashed_user_id"
   allowed_redirect_uris = [
     {
       matching_mode = "strict"
@@ -18,10 +30,11 @@ resource "authentik_provider_oauth2" "arcane_bom_arm" {
 
   property_mappings = [
     data.authentik_property_mapping_provider_scope.openid.id,
-    data.authentik_property_mapping_provider_scope.email.id,
+    # data.authentik_property_mapping_provider_scope.email.id,
     data.authentik_property_mapping_provider_scope.profile.id,
     data.authentik_property_mapping_provider_scope.entitlements.id,
-    authentik_property_mapping_provider_scope.groups.id,
+    # authentik_property_mapping_provider_scope.groups.id,
+    authentik_property_mapping_provider_scope.arcane_email.id,
   ]
 }
 
