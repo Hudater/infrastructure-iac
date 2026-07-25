a69c3037ae361cabbcb41ecd1c75b20aa67108bb
Harshit Mani Tripathi <harshit@hudater.dev>
2026-07-26 01:50:53 +0530

    fix 51820

--- oci/_modules/security.tf
+++ oci/_modules/security.tf
@@ -269,7 +269,7 @@ resource "oci_core_network_security_group_security_rule" "arm_netbird" {
   protocol                  = "17"
   source                    = "0.0.0.0/0"
   source_type               = "CIDR_BLOCK"
-  description               = "For Netbird"
+  description               = "For Netbird, please work"
 
   udp_options {
     destination_port_range {
